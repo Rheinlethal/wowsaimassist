9042f181b7a989d464684dda1fcfb8cdad37d97f
Rheinletha <farhanduta060402@gmail.com>
2025-12-29 22:24:24 +0700

    nambah requirement.txt

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ model = joblib.load("offset_predictor.pkl")
 st.title("Ini Aim Assist")
 
 # Input form
-shell_travel_time = st.number_input("Shell travel time (s)", value=0)
-distance = st.number_input("Distance (km)", value=0)
-angle = st.number_input("Angle (deg)", value=0)
-enemy_max_speed = st.number_input("Enemy max speed (knots)", value=0)
+shell_travel_time = st.number_input("Shell travel time (s)")
+distance = st.number_input("Distance (km)")
+angle = st.number_input("Angle (deg)")
+enemy_max_speed = st.number_input("Enemy max speed (knots)")
 
 # Predict button
 if st.button("Predict Offset_X"):
